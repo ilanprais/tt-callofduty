b80ba1e20d31e381102c1b0672801077be377124
ilandovprais <ilandovprais@gmail.com>
2023-02-22 16:54:40 +0200

    soldier service layer

---
+++ services/soldier.service.ts
@@ -0,0 +1,35 @@
+import { z } from "zod";
+import {
+  SoldierSchema,
+  Soldier,
+  SoldierQuery,
+} from "../services/schemas/soldier.zschema";
+import {
+  insertSoldier,
+  getSoldierByID,
+  getSoldiersByQuery,
+} from "../repositories/soldier.repository";
+import { DocumentNotFoundError } from "../error_handling/client_errors";
+
+const createSoldier = async (soldier: Soldier) => {
+  const DBResult = await insertSoldier(soldier);
+  const result = SoldierSchema.parse(DBResult);
+  return result;
+};
+
+const findSoldierByID = async (id: string) => {
+  const DBResult = await getSoldierByID(id);
+  if (DBResult === null) {
+    throw new DocumentNotFoundError(id);
+  }
+  const result = SoldierSchema.parse(DBResult);
+  return result;
+};
+
+const findSoldiersByQuery = async (query: SoldierQuery) => {
+  const DBResult = await getSoldiersByQuery(query);
+  const result = z.array(SoldierSchema).parse(DBResult);
+  return result;
+};
+
+export { createSoldier, findSoldierByID, findSoldiersByQuery };
