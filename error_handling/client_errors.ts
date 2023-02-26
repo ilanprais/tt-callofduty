4f67b42e54a58bb1ce20db47405b586273064dc2
ilandovprais <ilandovprais@gmail.com>
2023-02-26 15:18:01 +0200

    Custom errors

--- error_handling/client_errors.ts
+++ error_handling/client_errors.ts
@@ -1,7 +1,27 @@
-class DocumentNotFoundError extends Error {
-  constructor(request: string) {
-    super(`Document not found for: ${request}`);
+import { StatusCodes } from 'http-status-codes';
+
+class ClientError extends Error {
+  public code: StatusCodes;
+
+  constructor(request: string, code: StatusCodes) {
+    super(request);
+    this.code = code;
   }
 }
 
-export { DocumentNotFoundError };
+class DocumentNotFoundError extends ClientError {
+  constructor(request: string) {
+    super(`Document not found for: ${request}`, StatusCodes.NOT_FOUND);
+  }
+}
+
+class CannotModifyAssignedDutyError extends ClientError {
+  constructor(request: string) {
+    super(
+      `Cannot modify a duty which is assigned: ${request}`,
+      StatusCodes.BAD_REQUEST,
+    );
+  }
+}
+
+export { ClientError, DocumentNotFoundError, CannotModifyAssignedDutyError };
