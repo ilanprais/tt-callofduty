95f8a876270241d2df1b3cc1f3b5c77facc4f11b
ilandovprais <ilandovprais@gmail.com>
2023-03-23 15:14:27 +0200

    update app

--- app.ts
+++ app.ts
@@ -1,28 +1,9 @@
 import { startServer } from './index';
 import { closeConnection, startConnection } from './db_connection';
 
-import logger from './logger';
-import config from './config';
-import {
-  healthRoutes,
-  soldierRoutes,
-  dutyRoutes,
-  justiceBoardRoutes,
-} from './controllers';
-
-const app = express();
-
-app.use(morgan('common'));
-app.use(express.json());
-app.use('/health', healthRoutes);
-app.use('/soldiers', soldierRoutes);
-app.use('/duties', dutyRoutes);
-app.use('/justice-board', justiceBoardRoutes);
-
-const startApp = () => {
-  const listen = app.listen(config.SERVER_PORT);
-  logger.info(`Server started on port ${config.SERVER_PORT}`);
-  return listen;
+const startApp = async () => {
+  await startConnection();
+  startServer();
 };
 
 const closeApp = async () => {
