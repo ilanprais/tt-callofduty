f734d200bdad5a1d2ba7eb72114f55523f05a131
ilandovprais <ilandovprais@gmail.com>
2023-04-02 11:48:38 +0300

    scheduling test

--- tests/scheduling.test.ts
+++ tests/scheduling.test.ts
@@ -4,7 +4,7 @@ import { Express } from 'express';
 import { Collection } from 'mongodb';
 
 import { getServer } from '../index';
-import { Duty } from '../schemas/duty.zschema';
+import { Duty, DutySchema } from '../schemas/duty.zschema';
 import { Soldier } from '../schemas/soldier.zschema';
 import { createTestDutyWithId } from './data/duty.data';
 import { createTestSoldier } from './data/soldier.data';
@@ -25,23 +25,25 @@ beforeEach(async () => {
   dutiesCollection.deleteMany({});
 });
 
-afterEach(async () => {
-  soldiersCollection.deleteMany({});
-  dutiesCollection.deleteMany({});
-});
-
 describe('PUT duties/:id/schedule', () => {
   test('Should return code 200 and assign the soldier to the duty', async () => {
     const soldier = createTestSoldier(0);
-    const duty = createTestDutyWithId();
+    const duty = { ...createTestDutyWithId(), soldiersRequired: 1 };
     await soldiersCollection.insertOne(soldier);
     await dutiesCollection.insertOne(duty);
 
-    const res = await request(server).put(`duties/${duty._id}/schedule`);
+    const res = await request(server).put(`/duties/${duty._id}/schedule`);
 
     expect(res.statusCode).toEqual(200);
-    expect(res.body).toEqual({ ...duty, soldiers: [duty._id] });
-    const assignedSoldier = await request(server).get(`soldiers/${soldier.id}`);
-    expect(assignedSoldier.body.duties).toEqual([duty._id]);
+    expect(DutySchema.strip().parse(res.body)).toEqual(
+      DutySchema.strip().parse({
+        ...duty,
+        soldiers: [soldier.id],
+      }),
+    );
+    const assignedSoldier = await request(server).get(
+      `/soldiers/${soldier.id}`,
+    );
+    expect(assignedSoldier.body.duties).toEqual([duty._id.toString()]);
   });
 });
